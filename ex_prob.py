0addccbc54a091ca94786da61cf81c513d35b375
Marco Schivo <marcoschivo1@gmail.com>
2024-10-13 17:42:46 +0200

    added CDFs, added ex 3,4,5 of probability

--- ex_prob.py
+++ ex_prob.py
@@ -2,7 +2,6 @@ from statstoolkit.probability import *
 from statstoolkit.utils import *
 import matplotlib.pyplot as plt
 
-
 # ----------------------------- #
 # Example 1: Normal Distribution
 # ----------------------------- #
@@ -44,11 +43,76 @@ Z = Z.reshape(X.shape)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, cmap='viridis')
-
-# Add labels and title
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('Probability Density')
 ax.set_title('Multivariate Normal Distribution')
+
+# ----------------------------- #
+# Example 3: Generate a Custom PDF
+# ----------------------------- #
+
+# Define the PDF function
+def genpdf(x):
+    return 0.5 * (1 + x**3)
+
+# Generate values for x
+x = linspace(-1, 1, 100)
+y = genpdf(x)
+
+# Plot the PDF
+plt.figure()
+plt.plot(x, y, 'b', label='0.5 * (1 + x^3)')
+plt.plot(0.5 * x - 1.5, 0 * x, 'b-.')
+plt.plot(0.5 * x + 1.5, 0 * x, 'b-.')
+plt.axhline(0, color='black',linewidth=0.5)  # x-axis
+plt.axvline(0, color='black',linewidth=0.5)  # y-axis
+plt.xlabel('x')
+plt.ylabel('Probability Density')
+plt.legend()
+# plt.grid(True)
+
+# ----------------------------- #
+# Example 4: Hypergeometric Distribution
+# ----------------------------- #
+
+# Parameters
+M = 500  # Population size
+K = 30   # Number of successes in population
+n = 15   # Sample size
+
+# Generate values for x
+x = np.arange(0, 15)
+y = hygecdf(x, M, K, n)
+
+# Plot the CDF
+plt.figure()
+plt.plot(x, y, 'ro')
+plt.step(x, y)  # Stairs plot (commented out similar to the MATLAB code)
+plt.title('CDF of Hypergeometric')
+plt.grid(True)
+
+# ----------------------------- #
+# Example 5: Custom CDF
+# ----------------------------- #
+
+# Define the function gencf
+def gencf(x):
+    return 0.375 + 0.5 * x + 0.125 * x**4
+
+# Generate values for x
+x = linspace(-1, 1, 100)
+y = gencf(x)
+
+# Create the plot
+plt.figure()
+plt.plot(x, y, 'b', label='0.375 + 0.5*x + 0.125*x^4')
+plt.plot(0.5 * x - 1.5, 0 * x, 'b-.')
+plt.plot(0.5 * x + 1.5, (0 * x) + 1, 'b-.')
+plt.xlabel('x')
+plt.ylabel('Probability Density')
+plt.legend()
+plt.grid(True)
+
 
 plt.show()
--- statstoolkit/probability.py
+++ statstoolkit/probability.py
@@ -1,7 +1,7 @@
 from scipy import stats as st
 
 
-def binompdf(*args, **kwargs):
+def binopdf(*args, **kwargs):
     """Probability mass function at k of the given RV.
 
     Parameters
@@ -325,3 +325,292 @@ def mvnpdf(*args, **kwargs):
 
     """
     return st.multivariate_normal.pdf(*args, **kwargs)
+
+def binocdf(*args, **kwargs):
+    """Cumulative distribution function at k of the given RV.
+
+    Parameters
+    ----------
+    k : array_like
+        Quantiles.
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        Location parameter (default=0).
+
+    Returns
+    -------
+    cdf : array_like
+        Cumulative distribution function evaluated at k
+
+    """
+    return st.binom.cdf(*args, **kwargs)
+
+def poisscdf(*args, **kwargs):
+    """Cumulative distribution function at k of the given RV.
+
+    Parameters
+    ----------
+    k : array_like
+        Quantiles.
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        Location parameter (default=0).
+
+    Returns
+    -------
+    cdf : array_like
+        Cumulative distribution function evaluated at k
+
+    """
+    return st.poisson.cdf(*args, **kwargs)
+
+def geocdf(*args, **kwargs):
+    """Cumulative distribution function at k of the given RV.
+
+    Parameters
+    ----------
+    k : array_like
+        Quantiles.
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        Location parameter (default=0).
+
+    Returns
+    -------
+    cdf : array_like
+        Cumulative distribution function evaluated at k
+
+    """
+    return st.geom.cdf(*args, **kwargs)
+
+def nbincdf(*args, **kwargs):
+    """Cumulative distribution function at k of the given RV.
+
+    Parameters
+    ----------
+    k : array_like
+        Quantiles.
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        Location parameter (default=0).
+
+    Returns
+    -------
+    cdf : array_like
+        Cumulative distribution function evaluated at k
+
+    """
+    return st.nbinom.cdf(*args, **kwargs)
+
+def hygecdf(*args, **kwargs):
+    """Cumulative distribution function at k of the given RV.
+
+    Parameters
+    ----------
+    k : array_like
+        Quantiles.
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        Location parameter (default=0).
+
+    Returns
+    -------
+    cdf : array_like
+        Cumulative distribution function evaluated at k
+
+    """
+    return st.hypergeom.cdf(*args, **kwargs)
+
+def betacdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.beta.cdf(*args, **kwargs)
+
+def chi2cdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.chi2.cdf(*args, **kwargs)
+
+def expcdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.expon.cdf(*args, **kwargs)
+
+def fcdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.f.cdf(*args, **kwargs)
+
+def normcdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.norm.cdf(*args, **kwargs)
+
+def logncdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.lognorm.cdf(*args, **kwargs)
+
+def tcdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.t.cdf(*args, **kwargs)
+
+def wblcdf(*args, **kwargs):
+    """Cumulative distribution function at x of the given RV.
+
+    Parameters
+    ----------
+    x : array_like
+        quantiles
+    arg1, arg2, arg3,... : array_like
+        The shape parameter(s) for the distribution (see docstring of the
+        instance object for more information)
+    loc : array_like, optional
+        location parameter (default=0)
+    scale : array_like, optional
+        scale parameter (default=1)
+
+    Returns
+    -------
+    cdf : ndarray
+        Cumulative distribution function evaluated at x
+
+    """
+    return st.weibull_min.cdf(*args, **kwargs)
