7f5cb27468679ed3628661fc079ec37b3c09a7a0
alber <alberto.biscalchin@gmail.com>
2024-10-26 16:47:44 +0200

    publication setup files

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Statistical Methods for Data Science Toolkit'
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="statsToolkit",
+    version=VERSION,
+    author="Eng. Marco Schivo and Eng. Alberto Biscalchin",
+    author_email="<biscalchin.mau.se@gmail.com>, <marcoschivo1@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'statsmodels',
+        'pingouin'
+    ],
+    keywords=[
+        "statistics", "data science", "machine learning", "data analysis",
+        "descriptive statistics", "probability distributions", "visualizations"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
---
+++ statstoolkit/__init__.py
@@ -0,0 +1,4 @@
+from .statistics import *
+from .probability import *
+from .utils import *
+from .visualization import *
